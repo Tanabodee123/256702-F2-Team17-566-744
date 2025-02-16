01eee216c6f40455527cdfd8c2ddf69934a8d723
Saran Churungrueangkun <saran.chur@ku.th>
2025-02-16 22:19:32 +0700

    เพิ่มระบบเลือด การเสียเลือด คะแนน และการชนศัตรู

--- king_slime/src/main/java/com/project/App.java
+++ king_slime/src/main/java/com/project/App.java
@@ -1,15 +1,20 @@
 package com.project;
 
+import java.util.Map;
+
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.input.UserAction;
 
 import javafx.scene.input.KeyCode;
+import javafx.scene.text.Text;
+import javafx.util.Duration;
 
 public class App extends GameApplication {
     public static Player player;
     public static Enemy enemy;
+    private PhysicsManager physics;
 
     public static void main(String[] args) {
         launch(args);
@@ -28,58 +33,79 @@ public class App extends GameApplication {
     protected void initGame() {
         player = new Player();
         enemy = new Enemy();
+        physics = new PhysicsManager(player);
         player.createPlayer();
         enemy.spawnEnemies(5, player);
+
+        FXGL.run(() -> {
+            FXGL.inc("score", 1);
+        }, Duration.seconds(1));
+
+        FXGL.run(() -> {
+            int newEnemyCount = FXGL.geti("enemyCount") + 1;
+            FXGL.set("enemyCount", newEnemyCount);
+            enemy.spawnEnemies(newEnemyCount, player);
+        }, Duration.seconds(10));
     }
 
     @Override
     protected void initInput() {
-        FXGL.getInput().addAction(new UserAction("Move Left") {
+        Movement("Move Left", KeyCode.A, -1, 0);
+        Movement("Move Right", KeyCode.D, 1, 0);
+        Movement("Move Up", KeyCode.W, 0, -1);
+        Movement("Move Down", KeyCode.S, 0, 1);
+    }
+
+    @Override
+    protected void initGameVars(Map<String, Object> vars) {
+        vars.put("playerHP", 100);
+        vars.put("score", 0);
+        vars.put("enemyCount", 3);
+    }
+
+    @Override
+    protected void initPhysics() {
+        physics.init();
+    }
+
+    @Override
+    protected void initUI() {
+        createUILabel("HP:", 30, 80, "playerHP", 65, 80);
+        createUILabel("Score:", 30, 50, "score", 75, 50);
+    }
+
+    private void createUILabel(String label, double labelX, double labelY, String property, double valueX,
+            double valueY) {
+        Text labelText = new Text(label);
+        Text valueText = new Text();
+
+        labelText.setTranslateX(labelX);
+        labelText.setTranslateY(labelY);
+        valueText.setTranslateX(valueX);
+        valueText.setTranslateY(valueY);
+
+        if (FXGL.getWorldProperties().exists(property)) {
+            try {
+                valueText.textProperty().bind(FXGL.getWorldProperties().booleanProperty(property).asString());
+            } catch (Exception e) {
+                valueText.textProperty().bind(FXGL.getWorldProperties().intProperty(property).asString());
+            }
+        }
+        FXGL.getGameScene().addUINode(labelText);
+        FXGL.getGameScene().addUINode(valueText);
+    }
+
+    private void Movement(String name, KeyCode key, int dx, int dy) {
+        FXGL.getInput().addAction(new UserAction(name) {
             @Override
             protected void onAction() {
-                player.movePlayer(-1, 0);
+                player.movePlayer(dx, dy);
             }
 
             @Override
             protected void onActionEnd() {
                 player.stopPlayer();
             }
-        }, KeyCode.A);
-
-        FXGL.getInput().addAction(new UserAction("Move Right") {
-            @Override
-            protected void onAction() {
-                player.movePlayer(1, 0);
-            }
-
-            @Override
-            protected void onActionEnd() {
-                player.stopPlayer();
-            }
-        }, KeyCode.D);
-
-        FXGL.getInput().addAction(new UserAction("Move Up") {
-            @Override
-            protected void onAction() {
-                player.movePlayer(0, -1);
-            }
-
-            @Override
-            protected void onActionEnd() {
-                player.stopPlayer();
-            }
-        }, KeyCode.W);
-
-        FXGL.getInput().addAction(new UserAction("Move Down") {
-            @Override
-            protected void onAction() {
-                player.movePlayer(0, 1);
-            }
-
-            @Override
-            protected void onActionEnd() {
-                player.stopPlayer();
-            }
-        }, KeyCode.S);
+        }, key);
     }
 }
---
+++ king_slime/src/main/java/com/project/PhysicsManager.java
@@ -0,0 +1,88 @@
+package com.project;
+
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.physics.CollisionHandler;
+import javafx.util.Duration;
+import java.util.function.BiConsumer;
+
+public class PhysicsManager {
+    private boolean isShieldActive = false;
+    private int potionTimer = 0;
+    private double playerSpeed = 1.0;
+    private Player player;
+
+    public PhysicsManager(Player player) {
+        this.player = player;
+    }
+
+    public void init() {
+        addPhysics(EntityType.PLAYER, EntityType.ENEMY, this::Enemy);
+        /*
+         * addCollisionHandler(EntityType.PLAYER, EntityType.POTION, this::Potion);
+         * addCollisionHandler(EntityType.PLAYER, EntityType.MEAT, this::Meat);
+         * addCollisionHandler(EntityType.PLAYER, EntityType.SHIELD, this::Shield);
+         */
+    }
+
+    private void addPhysics(EntityType typeA, EntityType typeB, BiConsumer<Entity, Entity> handler) {
+        FXGL.getPhysicsWorld().addCollisionHandler(new CollisionHandler(typeA, typeB) {
+            @Override
+            protected void onCollision(Entity a, Entity b) {
+                handler.accept(a, b);
+            }
+        });
+    }
+
+    private void Enemy(Entity player, Entity enemy) {
+        if (isShieldActive && player.getPosition().distance(enemy.getPosition()) < 15) {
+            enemy.removeFromWorld();
+        } else if (player.getPosition().distance(enemy.getPosition()) < 15) {
+            FXGL.inc("playerHP", -20);
+            enemy.removeFromWorld();
+            if (FXGL.geti("playerHP") <= 0) {
+                FXGL.showMessage("Game Over", () -> FXGL.getGameController().gotoMainMenu());
+            }
+        }
+    }
+
+    /* private void Potion(Entity player, Entity potion) {
+        if (player.getPosition().distance(potion.getPosition()) < 30) {
+            potion.removeFromWorld();
+            if (potionTimer == 0) {
+                playerSpeed += 0.5;
+            }
+            potionTimer += 5;
+            FXGL.set("potionTime", potionTimer);
+
+            FXGL.run(() -> {
+                potionTimer--;
+                FXGL.set("potionTime", potionTimer);
+                if (potionTimer <= 0)
+                    playerSpeed = Math.max(1.0, playerSpeed - 0.5);
+            }, Duration.seconds(1), 5);
+        }
+    }
+
+    private void Meat(Entity player, Entity meat) {
+        if ((player.getPosition().distance(meat.getPosition()) < 30)) {
+            FXGL.inc("playerHP", 20);
+            meat.removeFromWorld();
+        }
+    }
+
+    private void Shield(Entity player, Entity shield) {
+        if ((player.getPosition().distance(shield.getPosition()) < 30)) {
+            shield.removeFromWorld();
+            isShieldActive = true;
+            FXGL.set("isShieldActive", true);
+            FXGL.showMessage("Shield Activated!");
+
+            FXGL.runOnce(() -> {
+                isShieldActive = false;
+                FXGL.set("isShieldActive", false);
+                FXGL.showMessage("Shield Expired!");
+            }, Duration.seconds(5));
+        }
+    } */
+}
